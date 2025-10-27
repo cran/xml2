8203c345d1fe55089c6e2755fa2d5ab9bbd76b71
Jeroen Ooms <jeroenooms@gmail.com>
2025-10-27 06:10:02 +0000

    version 1.4.1

--- tests/testthat/test-xml_write.R
+++ tests/testthat/test-xml_write.R
@@ -1,7 +1,10 @@
 test_that("write_xml errors for incorrect directory and with invalid inputs", {
   x <- read_xml("<x/>")
   filename <- "does_not_exist/test.xml"
-  expect_error(write_xml(x, filename), "'does_not_exist' does not exist in current working directory")
+  expect_error(
+    write_xml(x, filename),
+    "'does_not_exist' does not exist in current working directory"
+  )
 
   expect_snapshot(error = TRUE, {
     write_xml(x, c("test.xml", "foo"))
@@ -23,7 +26,10 @@ test_that("write_xml works with no options", {
   filename <- "../test.xml"
   on.exit(unlink(filename))
   write_xml(x, filename, options = NULL)
-  expect_identical(readChar(filename, 1000L), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<x/>\n")
+  expect_identical(
+    readChar(filename, 1000L),
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<x/>\n"
+  )
 })
 
 test_that("write_xml works with an explicit connections", {
@@ -112,22 +118,6 @@ test_that("write_xml works with node input and connections", {
     close(file)
   })
   expect_identical(readChar(file, 1000L), "<y/>")
-})
-
-test_that("write_html work with html input", {
-  x <- read_html("<html><title>Foo</title></html>")
-
-  filename <- "../test.html.gz"
-  write_html(x, filename)
-  file <- gzfile(filename, "rb")
-  on.exit({
-    unlink(filename)
-    close(file)
-  })
-  expect_identical(
-    readChar(file, 1000L),
-    "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/REC-html40/loose.dtd\">\n<html><head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n<title>Foo</title>\n</head></html>\n"
-  )
 })
 
 test_that("write_xml returns invisibly", {
